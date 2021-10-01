f29fed9a1b3d895bd951b42afb68992005bf4a33
James Carlson <jxxcarlson@gmail.com>
2021-10-01 00:29:50 -0400

    Enable import for Dylan

--- src/View/Large.elm
+++ src/View/Large.elm
@@ -313,7 +313,7 @@ footer model =
         ]
         [ View.Utility.showIf (Maybe.map .username model.currentUser == Just "jxxcarlson") (Button.adminPopup model)
         , View.Utility.hideIf (model.currentUser == Nothing) Button.exportJson
-        , View.Utility.showIf (Maybe.map .username model.currentUser == Just "dylan") Button.importJson
+        , View.Utility.showIf (Maybe.map .username model.currentUser == Just "dylan" || True) Button.importJson
         , messageRow model
         ]
 
